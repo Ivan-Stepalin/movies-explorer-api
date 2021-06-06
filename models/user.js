e07b11e4aa1b0a2b7c512588888d801dcdd40429
Ivan-Stepalin <iMmj7xPgMLz5Vji>
2021-06-06 13:23:11 +0300

    feat: add user Schema

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v)=>isEmail(v),
+      message: `Неправильный формат почты`
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false
+  }
+})
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Not found'));
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Not found'));
+          }
+          return user;
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema)
