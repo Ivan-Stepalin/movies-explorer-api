19be88a2203bcaca284d3312324eafdb69262034
ivan-stepalin <uncabron121@yandex.ru>
2021-08-24 14:54:49 +0300

    config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
 const {
-  JWT_SECRET_PHRASE = 'dev-secret',
+  JWT_SECRET = 'dev-secret',
   PORT = 3000,
   MONGO_URL = 'mongodb://localhost:27017/bitfilmsdb',
 } = process.env;
 
-module.exports = { JWT_SECRET_PHRASE, PORT, MONGO_URL };
+module.exports = { JWT_SECRET, PORT, MONGO_URL };
